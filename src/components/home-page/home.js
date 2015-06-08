a263bd7f230a0a4d2cc85821fae8623a17b93aeb
Vince Lee <vincent.lee@ao.com>
2015-06-08 14:09:35 +0100

    can refresh inhabitants

--- src/components/home-page/home.js
+++ src/components/home-page/home.js
@@ -24,7 +24,7 @@ define(["knockout", "jquery", "moment", "lodash", "text!./home.html"],
 	var newUsers = [
 		{
 			name: "Vince Lee",
-			refreshDateTime: "2015-06-08 12:40",
+			refreshDateTime: moment(),
 			regions: ["Food Counter"],
 			closestRegion: "Food Counter",
 			regionSignalStrength: 1.5,
@@ -32,7 +32,7 @@ define(["knockout", "jquery", "moment", "lodash", "text!./home.html"],
 		},
 		{
 			name: "Raj Eridisinghe",
-			refreshDateTime: "2015-06-08 12:40",
+			refreshDateTime: moment(),
 			regions: ["Telephone Booth"],
 			closestRegion: "Telephone Booth",
 			regionSignalStrength: 1.8,
@@ -50,6 +50,7 @@ define(["knockout", "jquery", "moment", "lodash", "text!./home.html"],
 	var Inhabitant = function(name, refreshDateTime, regions, closestRegion) {
 		var self = this
 		self.Name = name
+		self.EnteredAreaDateTime = moment(refreshDateTime).startOf('minute').fromNow()
 		self.RefreshDateTime = moment(refreshDateTime).startOf('minute').fromNow()
 		self.Regions = regions // string array
 		self.ClosestRegion = closestRegion
@@ -57,10 +58,10 @@ define(["knockout", "jquery", "moment", "lodash", "text!./home.html"],
 
   function HomeViewModel(route) {
   	var self = this
-  	self.inhabitants = _.map(oldUsers, function(user) {
+  	self.inhabitants = ko.observableArray(_.map(oldUsers, function(user) {
   		return new Inhabitant(user.name, user.refreshDateTime, user.regions, 
   			new Region(user.UUID, user.closestRegion, user.regionSignalStrength))
-  	})
+  	}))
 
   	self.showElement = function(elem) { console.log("show " + elem); if (elem.nodeType === 1) $(elem).hide().fadeIn("fast") }
     self.hideElement = function(elem) { console.log("hide " + elem); if (elem.nodeType === 1) $(elem).fadeOut("fast", function() { $(elem).remove(); }) }
@@ -82,8 +83,28 @@ define(["knockout", "jquery", "moment", "lodash", "text!./home.html"],
     	self.inhabitants(inhabitants) 
     }
 
-    self.refresh = function() {
+    self.refresh = function(newData) {
+    	if(!_.isArray(newData)) { newData = newUsers }
+    	// remove people no longer in feed
+    	var toKeep = _.filter(self.inhabitants(), function(o) {
+    		var match = _.find(newData, { 'name': o.Name })
+    		return match !== undefined
+    	})
     	
+    	self.inhabitants(toKeep)
+
+
+    	// add
+    	var toAdd = _.filter(newData, function(o) {
+    		var match = _.find(self.inhabitants(), { 'Name': o.name })
+    		return match === undefined
+    	})
+    	_.forEach(toAdd, function(a) {
+    		var curr = self.inhabitants()
+    		curr.push(new Inhabitant(a.name, a.refreshDateTime, a.regions, 
+    			new Region(a.UUID, a.Name, a.SignalStrength)))
+    		self.inhabitants(curr)
+    	})
     }
   }
 
